642eeb08386a7e70a0940638043ad15186010ea3
Kevin von Spiczak <caevv@me.com>
2019-04-16 20:05:10 +0200

    clean up

--- Scenes/UI/HSlider.gd
+++ Scenes/UI/HSlider.gd
@@ -1,16 +1,8 @@
 extends HSlider
 
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	min_value = 0.0001
 	step = 0.0001
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_HSlider_value_changed(value):
+func _on_HSlider_value_changed(value : float) -> void:
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), log(value) * 20)
