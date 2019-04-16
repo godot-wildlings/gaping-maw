bedefabefdaa493b0dc82f348778890a974aa706
Kevin von Spiczak <caevv@me.com>
2019-04-16 19:50:51 +0200

    fixes #1

--- Scenes/UI/CheckButton.gd
+++ Scenes/UI/CheckButton.gd
@@ -1,17 +1,5 @@
 extends CheckButton
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_CheckButton_toggled(button_pressed):
-	OS.set_window_fullscreen(true)
+func _on_CheckButton_toggled(button_pressed : bool) -> void:
+	print(button_pressed)
+	OS.set_window_fullscreen(button_pressed)
