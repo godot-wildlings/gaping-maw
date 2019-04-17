530047e6dce5b521b3fe55ae6dbdbec2365bac7a
plexsoup <plexsoup@gmail.com>
2019-04-17 02:10:30 -0700

    confine mouse in fullscreen mode

--- Scenes/UI/FullscreenButton.gd
+++ Scenes/UI/FullscreenButton.gd
@@ -5,3 +5,7 @@ func _ready():
 
 func _on_FullscreenButton_toggled(button_pressed : bool) -> void:
 	OS.set_window_fullscreen(button_pressed)
+	if button_pressed:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
